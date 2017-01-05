8749fce8d9e17a0c75a8a2fd72cc3f8afb55a72f
周辉 <396747973@qq.com>
2017-01-05 16:03:39 +0800

    Add files via upload

--- application/core/MY_Document.php
+++ application/core/MY_Document.php
@@ -3,6 +3,16 @@ class MY_Document {
 	public $title;
 	public $description;
 	public $keywords;
-	public $scripts;
-	public $styles;
+	public $scripts = array();
+	public $styles = array();
+
+	public function addScript($href)
+	{
+		$this->scripts[] = $href;
+	}
+
+	public function addStyle($href)
+	{
+		$this->styles[] = $href;
+	}
 }
